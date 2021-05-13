0f76f959de9a6c8d03b3df6bcc691a80023d50b6
Mohamadelattar <mohamadelattar99@gmail.com>
2021-05-13 06:34:11 +0000

    Commencer le travail sur la partie de séparation des Logins

--- Bricole-Front/src/app/login/login.component.ts
+++ Bricole-Front/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClientService } from 'src/service/client.service';
 
@@ -9,12 +9,14 @@ import { ClientService } from 'src/service/client.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
   loginForm = new FormGroup({
     'email' : new FormControl(null, [Validators.required, Validators.email]),
       'password' : new FormControl(null, [Validators.required, Validators.minLength(5)])
     });
   serverErrors = [];
-
+  isForm = true;
+  accountType : string;
   constructor(
     private clientService:ClientService,
     private router:Router,
@@ -24,23 +26,34 @@ export class LoginComponent implements OnInit {
   }
 
   login()
-  {
-   
+  {     
+      /*
       this.clientService.login(this.loginForm.value).subscribe(res => {
         localStorage.setItem('token',res[0]);
         localStorage.setItem('id',res[1].id);
         localStorage.setItem('nom',res[1].nom);
         localStorage.setItem('prenom',res[1].prenom);
         localStorage.setItem('email',res[1].email);
-        
-         
-
         if(res[0])
         {
-            this.router.navigate(['client-dash'])
+            this.router.navigate(['client-dash']);
         }
+      });  */
+  }
 
-      });  
+  freelancer(){
+    
+    this.accountType = "Freelancer";
+    console.log(this.accountType);
+    this.isForm = false;
+  }
+
+  client () {
+    
+    this.accountType = "Client";
+
+    console .log(this.accountType);
+    this.isForm = false;
   }
 
 }
